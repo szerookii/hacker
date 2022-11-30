475ebe57a05d75eda24e2703a81742266d11b3aa
szeroki <Seylio@outlook.com>
2022-11-30 23:04:50 +0100

    Add files

---
+++ scripts/Maze.gd
@@ -0,0 +1,9 @@
+extends Node
+
+class_name Maze
+
+onready var player : = $Player
+
+func _on_Level1_level_completed():
+	player.queue_free()
+	get_tree().change_scene("res://scenes/DialogueBankRob.tscn")

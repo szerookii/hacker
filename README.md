f84eb4e04380c70bea3a9f1c232ef75b047a7ce4
szerookii <36236750+szerookii@users.noreply.github.com>
2023-02-08 14:32:23 +0000

    Deploying to main from  @ aacba03851d86126a50872207e26e2c5b2fa6fc9 🚀

--- README.md
+++
@@ -1,2 +0,0 @@
-# hacker
-A game made for a school project. Made with the Godot Engine.
--- scripts/Button.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-export(PackedScene) var scene;
-
-func _pressed():
-	return get_tree().change_scene_to(scene)
